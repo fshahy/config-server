9f09817bf50d38e6fc00a32cf6bb1edee32a7a0b
Farid Shahy <fshahy@gmail.com>
2020-11-05 11:25:49 +0330

    init maven project.

---
+++ src/main/java/ir/delifery/configserver/FoodDeliFeryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package ir.delifery.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliFeryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliFeryConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/delifery/configserver/FoodDeliFeryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.delifery.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliFeryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

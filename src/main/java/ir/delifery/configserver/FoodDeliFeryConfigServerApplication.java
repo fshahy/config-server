492ee6f9b84a8870e270ff18755bcff8084e0171
Farid Shahy <fshahy@gmail.com>
2020-11-05 13:46:37 +0330

    replace application properties with bootstrap.

--- src/main/java/ir/delifery/configserver/FoodDeliFeryConfigServerApplication.java
+++ src/main/java/ir/delifery/configserver/FoodDeliFeryConfigServerApplication.java
@@ -2,8 +2,10 @@ package ir.delifery.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class FoodDeliFeryConfigServerApplication {
 
 	public static void main(String[] args) {
